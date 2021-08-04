d4c51163e5873c79794b0bb9f7dd413b7c96660d
unknown <rafa.daitx.silva@rede.ulbra.br>
2021-08-04 10:55:49 -0300

    Projeto Final

---
+++ conversor/android/app/src/main/kotlin/com/daitiks/conversor/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.daitiks.conversor
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ conversor/lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:async';
+import 'dart:convert';
+import 'home.dart';
+
+const request = "https://api.hgbrasil.com/finance?format=json-cors&key=93be48f8";
+
+void main() async{
+
+ print(await getData());
+  runApp(MaterialApp(
+    home: Home(),
+    theme: ThemeData(
+        hintColor: Colors.amber,
+        primaryColor: Colors.white,
+        inputDecorationTheme: InputDecorationTheme(
+          enabledBorder:
+          OutlineInputBorder(borderSide: BorderSide(color: Colors.white)),
+          focusedBorder:
+          OutlineInputBorder(borderSide: BorderSide(color: Colors.amber)),
+          hintStyle: TextStyle(color: Colors.amber),
+        )),
+  ));
+}
+
+Future<Map> getData() async{
+  http.Response response = await http.get(request);
+  return json.decode(response.body);
+}
+
+
